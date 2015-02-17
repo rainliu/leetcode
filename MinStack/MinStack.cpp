10210a7a69e29fab3973e8133052ef6628316374
Rain Liu <yuliu@outlook.com>
2015-02-17 11:02:25 -0800

    add Min Stack

--- MinStack/MinStack.cpp
+++ MinStack/MinStack.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <list>
 #include <algorithm>
 
 using namespace std;
@@ -13,43 +14,123 @@ pop() -- Removes the element on top of the stack.
 top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
 */
-
-class MinStack {
+class MinStack4 {
 public:
     void push(int x) {
-        mainstack.push(x);
+        mainstack.push_back(x);
         if(ministack.size()==0){
-            ministack.push(x);
+            ministack.push_back(x);
         }else{
-            if(x<ministack.top()){
-                ministack.push(x);        
+            if(x<ministack.back()){
+                ministack.push_back(x);        
             }else{
-                ministack.push(ministack.top());
+                ministack.push_back(ministack.back());
             }
         }
     }
 
     void pop() {
-        mainstack.pop();
-        ministack.pop();
+        mainstack.pop_back();
+        ministack.pop_back();
     }
 
     int top() {
-        return mainstack.top();
+        return mainstack.back();
     }
 
     int getMin() {
-        return ministack.top();
+        return ministack.back();
     }
 
 private:
-    stack<int> mainstack;
-    stack<int> ministack;
+    list<int> mainstack;
+    list<int> ministack;
 };
 
+class MinStack3 {
+public:    
+    MinStack3(){
+        m_top = -1;
+        m_capacity = 0;
+    }
+public:
+    void push(int x) {
+        if(m_top+1<m_capacity){
+            mainstack[m_top+1]=x;
+            if(x<ministack[m_top+1]){
+                ministack[m_top+1]=x;        
+            }else{
+                ministack[m_top+1]=ministack[m_top];
+            }
+        }else{
+            mainstack.push_back(x);
+            if(m_top==-1){
+                ministack.push_back(x);
+            }else{
+                if(x<ministack[m_top]){
+                    ministack.push_back(x);        
+                }else{
+                    ministack.push_back(ministack[m_top]);
+                }
+            }
+            ++m_capacity;
+        }
+        ++m_top;
+    }
+
+    void pop() {
+        --m_top;
+    }
+
+    int top() {
+        return mainstack[m_top];
+    }
+
+    int getMin() {
+        return ministack[m_top];
+    }
+
+private:
+    int m_top;
+    int m_capacity;
+    vector<int> mainstack;
+    vector<int> ministack;
+};
+
+class MinStack {  
+public:  
+    void push(int x) {  
+        S.push(x);  
+        if(MinS.empty() || x<=MinS.top()){ 
+            MinS.push(x);  
+        }
+    }  
+  
+    void pop(){  
+        int temp=S.top();  
+        S.pop();  
+        if(temp == MinS.top()){
+            MinS.pop();  
+        }
+    }  
+  
+    int top(){  
+        return S.top();  
+    }  
+
+    int getMin(){  
+        return MinS.top();  
+    }  
+
+private:  
+    stack<int> S;
+    stack<int> MinS;  
+}; 
+
+
 int main(){
-	MinStack s;
-	
+    MinStack s;
+    
     s.push(1); cout<<s.getMin()<<"\n";
     s.push(4); cout<<s.getMin()<<"\n";
     s.push(3); cout<<s.getMin()<<"\n";
@@ -58,5 +139,5 @@ int main(){
     s.pop();   cout<<s.getMin()<<"\n";
     s.pop();   cout<<s.getMin()<<"\n";
     s.pop();   cout<<s.getMin()<<"\n";
-	return 0;
+    return 0;
 }
