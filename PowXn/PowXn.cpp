4f7eef579aec4159d790dad7116d37a859385b8e
Rain Liu <yliu@live.com>
2015-02-18 00:40:23 -0800

    add POW(X,N)

---
+++ PowXn/PowXn.cpp
@@ -0,0 +1,46 @@
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/*
+Implement pow(x, n).
+*/
+
+class Solution {
+public:
+    double pow(double x, int n) {
+    	if(n==numeric_limits<int>::min()) return 1.0/(pow(x,numeric_limits<int>::max())*x);
+    	else if(n<0) return 1.0/pow(x,-n);
+        else if(n==0||x==1) return 1;
+        else if(n==1) return x;
+        else{
+	        double y = pow(x, n>>1);
+	        	
+	        if(n&0x1){//odd
+	        	return x*y*y;
+	        }else{//even
+	        	return y*y;
+	        }
+	    }
+    }
+};
+
+
+int main(){
+	Solution s;
+
+	cout<<s.pow(3, -1)<<endl;
+	cout<<s.pow(3, 0)<<endl;
+	cout<<s.pow(3, 1)<<endl;
+	cout<<s.pow(3, 2)<<endl;
+	cout<<s.pow(3, 3)<<endl;
+	cout<<s.pow(3, 4)<<endl;
+
+	return 0;
+}
