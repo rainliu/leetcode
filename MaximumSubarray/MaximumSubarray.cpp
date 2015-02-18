a5f030021ebcdcd03716a5a094cd7bd1c7826c80
Rain Liu <yliu@live.com>
2015-02-17 18:30:13 -0800

    add Maximum Subarray

--- MaximumSubarray/MaximumSubarray.cpp
+++ MaximumSubarray/MaximumSubarray.cpp
@@ -18,49 +18,38 @@ More practice:
 If you have figured out the O(n) solution, 
 try coding another solution using the divide and conquer approach, which is more subtle.
 */
-/*
+
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
-    	int b = 0;
-    	int e = n-1;
-     	return findSubArray(A, b, e);   
-    }
+        if(n==0) return 0;
+        if(n==1) return A[0];
 
-    int findSubArray(int A[], int& b, int& e){
-    	if(b==e){
-    		return A[b];
-    	}
-    	int b1 = b;
-    	int e1 = (e+b)/2;
-    	int b2 = e1+1;
-    	int e2 = e;
-    	int left = findSubArray(A, b1, e1); cout<<"left:"<<left<<endl;
-    	int right= findSubArray(A, b2, e2); cout<<"right:"<<right<<endl;
-    	
-    	int mixed = left+right;
-		if(mixed>=left && mixed>=right){
-			b = b1;
-			e = e2;
-			return mixed;
-		}else if(left>=mixed && left>=right){
-			b = b1;
-			e = e1;
-			return left;
-		}else{
-			b = b2;
-			e = e2;
-			return right;
-		}
-    	
+        int result = numeric_limits<int>::min();
+        int E = 0;
+        for(int j=0; j<n; j++){
+            E += A[j];
+            if(E>result){
+                result = E;
+            }
+            if(E<0){
+                E = 0;
+            }
+        }
+        
+        return result;
     }
 };
-*/
 int main(){
 	Solution s;
 	
-	int A[9] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-	cout<<s.maxSubArray(A, 9)<<endl;
-
+    {
+	   int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+	   cout<<s.maxSubArray(A, 9)<<endl;
+    }
+    {
+       int A[] = {-2, 1};
+       cout<<s.maxSubArray(A, 2)<<endl;
+    }
 	return 0;
 }
