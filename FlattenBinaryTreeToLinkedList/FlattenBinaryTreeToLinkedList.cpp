2ee0fbeff5a44a040d5f8303d10e19edb75fb652
Rain Liu <yuliu@outlook.com>
2015-02-20 13:50:05 -0800

    add Flatten Binary Tree to linked list

---
+++ FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,83 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+click to show hints.
+*/
+
+// Definition for binary tree
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        if(root==nullptr) return;
+
+		flattenTree(root->right);
+		TreeNode* last = flattenTree(root->left);				
+		if(last!=nullptr){
+			last->right = root->right;
+			root->right = root->left;
+			root->left  = nullptr;
+		}
+	}
+
+	TreeNode *flattenTree(TreeNode *root){
+		if(root==nullptr) return nullptr;
+		
+		TreeNode* lastRight = flattenTree(root->right);
+		TreeNode* lastLeft  = flattenTree(root->left);				
+		if(lastLeft!=nullptr){
+			lastLeft->right = root->right;
+			root->right = root->left;
+			root->left  = nullptr;
+		}
+
+		if(lastLeft==nullptr&&lastRight==nullptr){
+			return root;
+		}else if(lastRight==nullptr){
+			return lastLeft;
+		}else{
+			return lastRight;
+		}
+	}
+};
+
+int main(){
+	return 0;
+}
