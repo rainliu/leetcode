123ce583d35bab5f7ee5c745e3079e9a730fb0aa
Rain Liu <yliu@live.com>
2015-02-21 15:48:27 -0800

    add 141

---
+++ 141LinkedListCycle/141LinkedListCycle.cpp
@@ -0,0 +1,71 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/*
+https://oj.leetcode.com/problems/linked-list-cycle/
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+    	if(head==nullptr) return false;
+    	
+    	ListNode *p = nullptr;
+    	ListNode *q = head;
+    	ListNode *r = q->next;
+
+    	while(r!=nullptr&&r!=head){
+    		q->next = p;
+    		p = q;
+    		q = r;
+    		r = r->next;
+    	}
+
+    	return r==head;
+    }
+};
+
+class Solution2 {
+public:
+    bool hasCycle(ListNode *head) {
+    	if(head==nullptr) return false;
+
+    	unordered_map<ListNode *, bool> m;
+
+    	do{
+    		if(m.find(head)!=m.end()){
+    			return true;
+    		}else{
+    			m[head] = false;
+    		}
+
+    		head=head->next;
+    	}while(head!=nullptr);
+
+    	return false;
+    }
+};
+
+int main(){
+	return 0;
+}
