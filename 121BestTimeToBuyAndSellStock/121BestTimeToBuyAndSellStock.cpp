ffbaa57a713e8469494d0936b8786b9a0fec1f61
Rain Liu <yliu@live.com>
2015-02-22 22:12:07 -0800

    add 122

--- 121BestTimeToBuyAndSellStock/121BestTimeToBuyAndSellStock.cpp
+++ 121BestTimeToBuyAndSellStock/121BestTimeToBuyAndSellStock.cpp
@@ -17,8 +17,8 @@ Say you have an array for which the ith element is the price of a given stock on
 If you were only permitted to complete at most one transaction 
 (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
-E[i, j] = max(E[j-1], diff(j, i), if diff(j, i)>=0
-        = 0                    	  if diff(j, i)<0
+E[i, j] = max(E[i, j-1], diff(i, j)), if diff(i, j)>=0
+        = 0,   and update i,		  otherwise
 */
 
 class Solution {
---
+++ 122BestTimeToBuyAndSellStockII/122BestTimeToBuyAndSellStockII.cpp
@@ -0,0 +1,55 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/*
+https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. 
+You may complete as many transactions as you like 
+(ie, buy one and sell one share of the stock multiple times). 
+However, you may not engage in multiple transactions at the same time 
+(ie, you must sell the stock before you buy again).
+
+E[i, j] = max(E[i, j-1]+diff(j-1, j), E[i, j-1], diff(i, j)),   if diff(i, j)>=0
+        = max(E[i, j-1]+diff(j-1, j), E[i, j-1]), and update i, otherwise
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+    	if(n<=1) return 0;
+
+        vector<int> E(n, 0);
+        int i = 0;
+        E[0] = 0;
+        for(int j=1; j<n; ++j){
+        	if(prices[j]-prices[i]>=0){
+        		E[j] = max(E[j-1], prices[j]-prices[i]);
+        	}else{
+        		E[j] = E[j-1];
+        		i = j;
+        	}
+        	if(prices[j]-prices[j-1]>=0){
+        		E[j] = max(E[j-1]+prices[j]-prices[j-1], E[j]);
+        	}
+        }
+
+        return E[n-1];
+    }
+};
+
+
+int main(){
+	return 0;
+}
