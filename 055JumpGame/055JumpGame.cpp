b590325dc352504a8c8cb0c944a7880bf1326501
Rain Liu <yliu@live.com>
2015-02-22 23:26:00 -0800

    add empty 055/152

---
+++ 055JumpGame/055JumpGame.cpp
@@ -0,0 +1,32 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/*
+https://oj.leetcode.com/problems/jump-game/
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+*/
+
+class Solution {
+public:
+    bool canJump(int A[], int n) {
+        
+    }
+};
---
+++ 152MaximumProductSubarray/152MaximumProductSubarray.cpp
@@ -0,0 +1,26 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+/*
+https://oj.leetcode.com/problems/maximum-product-subarray/
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6.
+*/
+
+class Solution {
+public:
+    int maxProduct(int A[], int n) {
+        
+    }
+};
