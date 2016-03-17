945a8de1630c9e6f1aa4727d257071a7498ba3a2
Rain Liu <yuliu@outlook.com>
2016-03-17 00:19:47 -0700

    update golang sort

--- leetcode.py
+++ leetcode.py
@@ -5,43 +5,61 @@ import string
 root = urllib2.urlopen("https://leetcode.com/problemset/algorithms/").read()
 prefix = 'https://leetcode.com'
 
-with open("leetcode.html", "w") as f:
-	f.write(root) 
+#with open("leetcode.html", "w") as f:
+#	f.write(root) 
 
 buf = StringIO.StringIO(root)
-line_1 = ''
-line_2 = ''
+
 golang_count = 0
-	
-with open("leetcode.csv", "w") as f:
-	for line_0 in buf.readlines():
+
+with open("leetcode_golang_support.csv", "w") as f:
+	f.write("#,URL,Title,Acceptance,Difficulty,Golang Support\n");
+
+	lines = buf.readlines()
+	for idx, line_0 in enumerate(lines):
 		isSubstring = "<a href=\"/problems/" in line_0
 		if isSubstring:
-			number = line_2.strip()
+			number = lines[idx-2].strip()
 			number = string.replace(number, '<td>', '');
 			number = string.replace(number, '</td>', '');
-			print number
+			#print number
+
+			extra_line_number = 0
+			isExtraLines = "fa fa-lock" in lines[idx+2]
+			if isExtraLines:
+				extra_line_number = 2
+
+			acceptance = lines[idx+3+extra_line_number].strip()
+			acceptance = string.replace(acceptance, '<td>', '');
+			acceptance = string.replace(acceptance, '</td>', '');
+			#print acceptance
+
+			difficulty = lines[idx+5+extra_line_number].strip()
+			difficulty = string.replace(difficulty, '</td>', '');
+			i = difficulty.index('>')
+			difficulty = string.replace(difficulty, difficulty[0:i+1], '');
+			#print difficulty
 
 			line = line_0.strip()
+			line = string.replace(line, ',', '') #remove ',' between pow(x,n)
 			line = string.replace(line, '<a href=\"', prefix)
 			line = string.replace(line, '\">', ',')
 			line = string.replace(line, '</a>', '')
 			split_fields = line.split(',')
-			print split_fields
+			#print split_fields
 
+			golang_exist = ''
 			problem = urllib2.urlopen(split_fields[0]).read()
 			buf2 = StringIO.StringIO(problem)
 			for line2 in buf2.readlines():
 				isSubstring2 = "golang" in line2
 				if isSubstring2:
-					line = line+",golang"
-					golang_count++
-					print "golang"
+					golang_exist = 'golang'
+					golang_count += 1
+					#print "golang"
 					break
 
-			f.write(number+","+line+"\n")
-		
-		line_2 = line_1
-		line_1 = line_0			
+			print number+","+line+","+acceptance+","+difficulty+","+golang_exist+"\n"
+			f.write(number+","+line+","+acceptance+","+difficulty+","+golang_exist+"\n")	
 
-print "Total golang count is: "+golang_count
+print "Total golang count is: "+str(golang_count)
