328f6a005a1d123e13bb0727e08cb5fb75609f8e
Rain Liu <yuliu@outlook.com>
2016-06-01 13:53:09 -0700

    update golang

--- leetcode.go
+++ leetcode.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"strings"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func main(){
+func main() {
 	if len(os.Args) != 2 {
 		println("Usage: leetcode language")
 		println("\t language: C/C++/golang/swift...")
@@ -20,35 +20,33 @@ func main(){
 	prefix := "https://leetcode.com"
 
 	resp, err := http.Get(url)
-	if err!=nil {
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 		return
 	}
 	root, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-	if err!=nil {
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 		return
 	}
 
-/*
 	fp_html, err := os.Create("leetcode.html")
-	if err!=nil{
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "Create file leetcode.html failed\n")
 		return
 	}
 	defer fp_html.Close()
 	fp_html.Write(root)
-*/
-	
-	fp_lang, err := os.Create("leetcode_"+lang+"_support.csv")
-	if err!=nil{
+
+	fp_lang, err := os.Create("leetcode_" + lang + "_support.csv")
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "Create file leetcode_lang_support.csv failed\n")
 		return
 	}
 	defer fp_lang.Close()
 
-	fp_lang.WriteString("#,URL,Title,Support,Difficulty,Locked\n");
+	fp_lang.WriteString("#,URL,Title,Support,Difficulty,Locked\n")
 
 	lang_count := 0
 	lines := strings.Split(string(root), "\n")
@@ -56,22 +54,27 @@ func main(){
 		isSubstring := strings.Contains(line_0, "<a href=\"/problems/")
 		if isSubstring {
 			number := strings.TrimSpace(lines[idx-2])
-			number = strings.Replace(number, "<td>", "", -1);
-			number = strings.Replace(number, "</td>", "", -1);
+			number = strings.Replace(number, "<td>", "", -1)
+			number = strings.Replace(number, "</td>", "", -1)
 			//print number
 
 			extra_line_number := 0
 			locked := ""
-			isExtraLines := strings.Contains(lines[idx+2],"fa fa-lock")
-			if isExtraLines{
+			isExtraLines := strings.Contains(lines[idx+2], "fa fa-lock")
+			if isExtraLines {
 				extra_line_number = 2
 				locked = "Locked"
 			}
 
-			difficulty := strings.TrimSpace(lines[idx+5+extra_line_number])
-			difficulty = strings.Replace(difficulty, "</td>", "", -1);
-			i := strings.Index(difficulty,">")
-			difficulty = strings.Replace(difficulty, difficulty[0:i+1], "", -1);
+			isEditorial := strings.Contains(lines[idx+6+extra_line_number], "true")
+			if isEditorial {
+				extra_line_number += 9
+			}
+
+			difficulty := strings.TrimSpace(lines[idx+10+extra_line_number])
+			difficulty = strings.Replace(difficulty, "</td>", "", -1)
+			i := strings.Index(difficulty, ">")
+			difficulty = strings.Replace(difficulty, difficulty[0:i+1], "", -1)
 			//print difficulty
 
 			line := strings.TrimSpace(line_0)
@@ -84,22 +87,22 @@ func main(){
 
 			//////////////////////////////////////////
 			resp2, err := http.Get(split_fields[0])
-			if err!=nil {
+			if err != nil {
 				fmt.Fprintf(os.Stderr, "fetch %s: %v\n", split_fields[0], err)
 				return
 			}
 			problem, err := ioutil.ReadAll(resp2.Body)
 			resp2.Body.Close()
-			if err!=nil {
+			if err != nil {
 				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", split_fields[0], err)
 				return
 			}
 
 			lang_exist := ""
 			buf2 := strings.Split(string(problem), "\n")
-			for _, line2 := range buf2{
+			for _, line2 := range buf2 {
 				isSubstring2 := strings.Contains(line2, lang)
-				if isSubstring2{
+				if isSubstring2 {
 					lang_exist = lang
 					lang_count += 1
 					//print "golang"
@@ -107,8 +110,8 @@ func main(){
 				}
 			}
 
-			print              (number+","+line+","+lang_exist+","+difficulty+","+locked+"\n")
-			fp_lang.WriteString(number+","+line+","+lang_exist+","+difficulty+","+locked+"\n")	
+			print(number + "," + line + "," + lang_exist + "," + difficulty + "," + locked + "\n")
+			fp_lang.WriteString(number + "," + line + "," + lang_exist + "," + difficulty + "," + locked + "\n")
 		}
 	}
 	println("Total Count: ", lang_count)
