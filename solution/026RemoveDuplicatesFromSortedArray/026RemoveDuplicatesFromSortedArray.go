3ea708229f2760d07d9c0b8fef66dbc9d122ec6a
Rain Liu <yuliu@outlook.com>
2017-05-21 13:35:36 -0700

    026RemoveDuplicatesFromSortedArray.go

---
+++ solution/026RemoveDuplicatesFromSortedArray/026RemoveDuplicatesFromSortedArray.go
@@ -0,0 +1,17 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	i, j := 0, 1
+	for j < len(nums) {
+		if nums[i] != nums[j] {
+			nums[i+1], nums[j] = nums[j], nums[i+1]
+			i++
+		}
+		j++
+	}
+	return i + 1
+}
+
+func main() {
+
+}
