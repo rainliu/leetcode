0963d1310d5cbf2c66aa7679d26c85e5bc6e7dcc
Rain Liu <yuliu@outlook.com>
2017-05-23 23:44:58 -0700

    add 053MaximumSubarray.go

--- solution/053MaximumSubarray/053MaximumSubarray.go
+++ solution/053MaximumSubarray/053MaximumSubarray.go
@@ -16,8 +16,10 @@ func maxSubArray(nums []int) int {
 	local := nums[0]
 	global := nums[0]
 	for i := 1; i < len(nums); i++ {
-		local = max(nums[i], local+nums[i])
-		global = max(local, global)
+		//local optimal must contain nums[i]
+		//so, if local<0, then only nums[i], otherwise nums[i]+local
+		local = max(nums[i], nums[i]+local)
+		global = max(global, local)
 	}
 	return global
 }
