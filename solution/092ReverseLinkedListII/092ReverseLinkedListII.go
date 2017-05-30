5ec8f7b6cd91762777a76684db523c844967d10c
Rain Liu <yuliu@outlook.com>
2017-05-29 22:43:23 -0700

    add 092ReverseLinkedListII.go

---
+++ solution/092ReverseLinkedListII/092ReverseLinkedListII.go
@@ -0,0 +1,34 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseBetween(head *ListNode, m int, n int) *ListNode {
+	if m >= n {
+		return head
+	}
+	dummy := ListNode{0, head}
+	p := &dummy
+	q := p.Next
+	for i := 1; i < m; i++ {
+		p = p.Next
+		q = q.Next
+	}
+	for i := m; i < n; i++ {
+		r := q.Next
+		o := p.Next
+		p.Next = r
+		q.Next = r.Next
+		r.Next = o
+	}
+	return dummy.Next
+}
+
+func main() {
+
+}
