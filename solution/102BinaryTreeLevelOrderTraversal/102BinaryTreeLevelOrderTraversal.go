b9d58ed018b9e7e10f00c8e36b2799adad4f050a
Rain Liu <yuliu@outlook.com>
2017-05-31 23:15:37 -0700

    add 102BinaryTreeLevelOrderTraversal.go

---
+++ solution/102BinaryTreeLevelOrderTraversal/102BinaryTreeLevelOrderTraversal.go
@@ -0,0 +1,61 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type Node struct {
+	t *TreeNode
+	l int
+}
+
+type Queue []Node
+
+func (q *Queue) Empty() bool { return len(*q) == 0 }
+func (q *Queue) Push(n Node) { *q = append(*q, n) }
+func (q *Queue) Pop() (n Node) {
+	n = (*q)[0]
+	*q = (*q)[1:]
+	return
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	var results [][]int
+	var result []int
+	var q Queue
+	q.Push(Node{root, 0})
+	last := -1
+	for !q.Empty() {
+		p := q.Pop()
+		if p.l != last {
+			if result != nil {
+				results = append(results, result)
+			}
+			last = p.l
+			result = nil
+		}
+		result = append(result, p.t.Val)
+		if p.t.Left != nil {
+			q.Push(Node{p.t.Left, p.l + 1})
+		}
+		if p.t.Right != nil {
+			q.Push(Node{p.t.Right, p.l + 1})
+		}
+	}
+	if result != nil {
+		results = append(results, result)
+	}
+	return results
+}
+
+func main() {
+
+}
