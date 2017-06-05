905e06161306bf5d809096888c88526e53c2bcda
Rain Liu <yuliu@outlook.com>
2017-06-04 21:03:54 -0700

    add 137SingleNumberII.go with bitwise

--- solution/137SingleNumberII/137SingleNumberII.go
+++ solution/137SingleNumberII/137SingleNumberII.go
@@ -14,6 +14,21 @@ func singleNumber(nums []int) int {
 	return 0
 }
 
+func singleNumber(nums []int) int {
+	n := len(nums)
+	one1 := 0
+	two1 := 0
+	thr1 := 0
+	for i := 0; i < n; i++ {
+		two1 |= one1 & nums[i]
+		one1 ^= nums[i]
+		thr1 = two1 & one1
+		one1 = one1 &^ thr1
+		two1 = two1 &^ thr1
+	}
+	return one1
+}
+
 func main() {
 
 }
