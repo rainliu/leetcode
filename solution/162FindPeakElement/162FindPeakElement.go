b705b26197b0b3f8b432e0b4e230fca8215c57f2
Rain Liu <yuliu@outlook.com>
2017-06-07 22:52:55 -0700

    add 162FindPeakElement.go

---
+++ solution/162FindPeakElement/162FindPeakElement.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"math"
+)
+
+func findPeakElement(nums []int) int {
+	n := len(nums)
+	global := math.MinInt32
+	index := 0
+	var left, right int
+	for i := 0; i < n; i++ {
+		if i == 0 {
+			left = math.MinInt32
+		} else {
+			left = nums[i-1]
+		}
+		if i == n-1 {
+			right = math.MinInt32
+		} else {
+			right = nums[i+1]
+		}
+		if nums[i] >= left && nums[i] <= right && nums[i] > global {
+			global = nums[i]
+			index = i
+		}
+	}
+	return index
+}
+
+func main() {
+
+}
