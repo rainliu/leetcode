585241097613b0af6987aecdd87746281512440e
Rain Liu <yuliu@outlook.com>
2017-06-15 00:21:06 -0700

    add 217ContainsDuplicate

---
+++ solution/217ContainsDuplicate/217ContainsDuplicate.cpp
@@ -0,0 +1,20 @@
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  bool containsDuplicate(vector<int> &nums) {
+    unordered_set<int> s;
+    for (int i = 0; i < nums.size(); i++) {
+      if (s.find(nums[i]) != s.end()) {
+        return true;
+      } else {
+        s.insert(nums[i]);
+      }
+    }
+    return false;
+  }
+};
+
+int main() {}
