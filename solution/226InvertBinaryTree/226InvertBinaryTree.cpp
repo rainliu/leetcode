b3b5900a5f1b35091755f7610fae7079912cefe3
Rain Liu <yuliu@outlook.com>
2017-06-15 23:14:21 -0700

    add 226InvertBinaryTree

---
+++ solution/226InvertBinaryTree/226InvertBinaryTree.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  TreeNode *invertTree(TreeNode *root) {
+    if (root != nullptr) {
+      TreeNode *tmp = root->left;
+      root->left = invertTree(root->right);
+      root->right = invertTree(root->left);
+    }
+    return root;
+  }
+};
+
+int main() { return 0; }
