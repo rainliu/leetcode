0ed1a8b5ce1194bf3b3ed73557367a580733d08e
Rain Liu <yuliu@outlook.com>
2017-06-17 00:32:07 -0700

    add voting solution for 169MajorityElement

--- solution/169MajorityElement/169MajorityElement.cpp
+++ solution/169MajorityElement/169MajorityElement.cpp
@@ -1,17 +1,19 @@
-#include <sstream>
+#include <algorithm>
 #include <iostream>
-#include <vector>
+#include <limits>
+#include <sstream>
 #include <stack>
 #include <unordered_map>
-#include <algorithm>
-#include <limits>
+#include <vector>
 
 using namespace std;
 
 /*
-Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+Given an array of size n, find the majority element. The majority element is the
+element that appears more than ⌊ n/2 ⌋ times.
 
-You may assume that the array is non-empty and the majority element always exist in the array.
+You may assume that the array is non-empty and the majority element always exist
+in the array.
 
 Credits:
 Special thanks to @ts for adding this problem and creating all test cases.
@@ -19,25 +21,41 @@ Special thanks to @ts for adding this problem and creating all test cases.
 
 class Solution {
 public:
-    int majorityElement(vector<int> &num) {
-    	unordered_map<int, int> memap;
-    	int halfn = num.size()/2;
-    	for(const int &x : num){
-    		if(memap.find(x)!=memap.end()){
-    			memap[x]++;
-    			if(memap[x]>halfn)
-    				return x;
-    		}else{
-    			memap[x]=1;
-    		}
-    	}    
-
-    	return num[0];
+  int majorityElement(vector<int> &num) {
+    unordered_map<int, int> memap;
+    int halfn = num.size() / 2;
+    for (const int &x : num) {
+      if (memap.find(x) != memap.end()) {
+        memap[x]++;
+        if (memap[x] > halfn)
+          return x;
+      } else {
+        memap[x] = 1;
+      }
     }
+
+    return num[0];
+  }
+  int majorityElement(vector<int> &num) {
+    int n = nums.size();
+    int m;
+    int i = 0;
+    for (auto x : nums) {
+      if (i == 0) {
+        m = x;
+        i = 1;
+      } else if (m == x) {
+        i++;
+      } else {
+        i--;
+      }
+    }
+    return m;
+  }
 };
 
-int main(){
-	Solution s;
+int main() {
+  Solution s;
 
-	return 0;
+  return 0;
 }
