10a317dff87bb9ca2271f71a330cdd6924b3e59c
Rain Liu <yuliu@outlook.com>
2017-06-16 23:55:32 -0700

    add 235LowestCommonAncestorOfABinarySearchTree

---
+++ solution/235LowestCommonAncestorOfABinarySearchTree/235LowestCommonAncestorOfABinarySearchTree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+    if (p->val > q->val) {
+      TreeNode *t = p;
+      p = q;
+      q = t;
+    }
+    while (root != nullptr) {
+      if (p->val == root->val || q->val == root->val) {
+        return root;
+      } else if (q->val < root->val) {
+        root = root->left;
+      } else if (p->val > root->val) {
+        root = root->right;
+      } else {
+        return root;
+      }
+    }
+    return nullptr;
+  }
+};
+
+int main() { return 0; }
