3952f71861ba66ab7f67ac7d5bbd4f461a04bb14
Rain Liu <yuliu@outlook.com>
2017-06-16 23:30:01 -0700

    add 234PalindromeLinkedList TO(N) and SO(1)

--- solution/234PalindromeLinkedList/234PalindromeLinkedList.cpp
+++ solution/234PalindromeLinkedList/234PalindromeLinkedList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 /**
@@ -12,7 +13,7 @@ struct ListNode {
 
 class Solution {
 public:
-  bool isPalindrome(ListNode *head) {
+  bool isPalindrome1(ListNode *head) {
     vector<int> p;
     while (head != nullptr) {
       p.push_back(head->val);
@@ -29,6 +30,54 @@ public:
     }
     return true;
   }
+
+  bool isPalindrome(ListNode *head) {
+    int n = 0;
+    ListNode *p = head;
+    while (p != nullptr) {
+      n++;
+      p = p->next;
+    }
+    if (n <= 1)
+      return true;
+    int mid = n / 2;
+    p = head;
+    while (mid != 0) {
+      p = p->next;
+      mid--;
+    }
+    // cout<<p->val;
+    ListNode *q = p->next;
+    while (q != nullptr) {
+      ListNode *r = q->next;
+      q->next = p;
+      p = q;
+      q = r;
+    }
+    q = head;
+    mid = n / 2;
+    while (mid) {
+      mid--;
+      // cout << mid << q->val << p->val << endl;
+      if (q->val != p->val) {
+        return false;
+      }
+      q = q->next;
+      // cout << mid;
+      p = p->next;
+      // cout << mid << endl;
+    }
+    return true;
+  }
 };
 
-int main() { return 0; }
+int main() {
+  Solution s;
+  ListNode *p1 = new ListNode(1);
+  ListNode *p2 = new ListNode(2);
+  ListNode *p3 = new ListNode(2);
+  p1->next = p2;
+  p2->next = p3;
+  cout << "it is" << s.isPalindrome(p1) << endl;
+  return 0;
+}
