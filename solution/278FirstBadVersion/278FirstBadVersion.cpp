19e3dbb2ef6fce4a66e889134591936b3d685c14
Rain Liu <yuliu@outlook.com>
2017-06-20 23:27:23 -0700

    add optimal 278

--- solution/278FirstBadVersion/278FirstBadVersion.cpp
+++ solution/278FirstBadVersion/278FirstBadVersion.cpp
@@ -7,21 +7,14 @@ bool isBadVersion(int version);
 class Solution {
 public:
   int firstBadVersion(int n) {
-    if (isBadVersion(1)) {
-      return 1;
-    }
     int lo = 1;
     int hi = n;
     while (lo < hi) {
-      if (hi - lo == 1 && !isBadVersion(lo) && isBadVersion(hi)) {
-        return hi;
+      int mi = lo + (hi - lo) / 2;
+      if (isBadVersion(mi)) {
+        hi = mi;
       } else {
-        int mi = lo + (hi - lo) / 2;
-        if (isBadVersion(mi)) {
-          hi = mi;
-        } else {
-          lo = mi;
-        }
+        lo = mi + 1;
       }
     }
     return lo;
