1d56c01d3387a8354a9f80165e939d852b75240c
Rain Liu <yuliu@outlook.com>
2017-06-25 22:46:13 -0700

    add 371SumOfTwoIntegers

---
+++ solution/371SumOfTwoIntegers/371SumOfTwoIntegers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+  int getSum(int a, int b) { return b == 0 ? a : getSum(a ^ b, (a & b) << 1); }
+};
+
+int main() { return 0; }
