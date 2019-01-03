665131c93a98884d921364d5b45ed439ef499caa
Rain Liu <yuliu@outlook.com>
2019-01-02 19:16:21 -0800

    Create 104MaximumDepthOfBinaryTree.rs

---
+++ solution/104MaximumDepthOfBinaryTree/104MaximumDepthOfBinaryTree.rs
@@ -0,0 +1,32 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(root) => {
+                let mut node = root.borrow_mut();
+                1 + std::cmp::max(Self::max_depth(node.left.take()), 
+                                  Self::max_depth(node.right.take()))
+            }
+            None => 0,
+        }    
+    }
+}
