c07d15495537ee909f0c89f20d58f1db85544313
Rain Liu <yuliu@outlook.com>
2019-01-06 20:42:07 -0800

    Update 687LongestUnivaluePath.rs

--- src/687LongestUnivaluePath/687LongestUnivaluePath.rs
+++ src/687LongestUnivaluePath/687LongestUnivaluePath.rs
@@ -20,26 +20,26 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn longest_univalue_path(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if let Some(node) = root {
+        if let Some(root) = root {
             let mut global = 0;
-            let mut p = node.borrow_mut();
-            let l = Self::longest_univalue_path_helper(p.left.take(), p.val, &mut global);
-            let r = Self::longest_univalue_path_helper(p.right.take(), p.val, &mut global);
-            std::cmp::max(global, l+r+1)-1
+            let mut node = root.borrow_mut();
+            let l = Self::longest_univalue_path_helper(node.left.take(), node.val, &mut global);
+            let r = Self::longest_univalue_path_helper(node.right.take(), node.val, &mut global);
+            std::cmp::max(global, l+r)
         }else{
             0
         }
     }
     
     fn longest_univalue_path_helper(root: Option<Rc<RefCell<TreeNode>>>, val: i32, global: &mut i32) -> i32 {
-        if let Some(node) = root {
-            let mut p = node.borrow_mut();
-            let l = Self::longest_univalue_path_helper(p.left.take(), if p.val == val {val} else {p.val}, global);
-            let r = Self::longest_univalue_path_helper(p.right.take(), if p.val == val {val} else {p.val}, global);
-            if p.val == val {
+        if let Some(root) = root {
+            let mut node = root.borrow_mut();
+            let l = Self::longest_univalue_path_helper(node.left.take(), if node.val == val {val} else {node.val}, global);
+            let r = Self::longest_univalue_path_helper(node.right.take(), if node.val == val {val} else {node.val}, global);
+            if node.val == val {
                 1+std::cmp::max(l, r)
             }else{
-                *global = std::cmp::max(*global, l+r+1);
+                *global = std::cmp::max(*global, l+r);
                 0
             }
         }else{
