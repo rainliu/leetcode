409a588b4461ebfeb7aa2161c19c6bc1f83bad00
Rain Liu <yuliu@outlook.com>
2019-01-07 21:17:11 -0800

    Create 876MiddleOfTheLinkedList.rs

---
+++ src/876MiddleOfTheLinkedList/876MiddleOfTheLinkedList.rs
@@ -0,0 +1,43 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut slow = &head;
+        let mut fast = &head;
+        while let Some(node) = fast.as_ref() {
+            if node.next.is_none() {
+                break;
+            }
+            slow = &slow.as_ref().unwrap().next;
+            fast = &node.next.as_ref().unwrap().next;
+        }
+        Self::clone_list_from_ref(slow)
+    }
+    
+    fn clone_list_from_ref(head: &Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() {return None;}
+        let mut dummy = ListNode::new(0);
+        let mut p = &mut dummy;
+        let mut q = head;
+        while let Some(node) = q.as_ref() {
+            p.next = Some(Box::new(ListNode::new(node.val)));
+            q = &node.next;
+            p = p.next.as_mut().unwrap();
+        }
+        dummy.next
+    }
+}
