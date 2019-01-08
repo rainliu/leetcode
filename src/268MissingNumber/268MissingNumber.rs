6762dd2a77d13bf8d39f325c40daa5e2b33cafe5
Rain Liu <yuliu@outlook.com>
2019-01-08 00:04:25 -0800

    Update 268MissingNumber.rs

--- src/268MissingNumber/268MissingNumber.rs
+++ src/268MissingNumber/268MissingNumber.rs
@@ -18,3 +18,23 @@ impl Solution {
         n
     }
 }
+
+impl Solution2 {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        let mut p = 0;
+        let mut n = nums.len();
+        while p < n {
+            if nums[p] == p as i32 {
+                p += 1;
+            }else if nums[p] == n as i32 {
+                nums.swap(p, n-1);
+                n -= 1;
+            }else{
+                let q = nums[p];
+                nums.swap(p, q as usize);
+            }
+        }
+        p as i32
+    }
+}
