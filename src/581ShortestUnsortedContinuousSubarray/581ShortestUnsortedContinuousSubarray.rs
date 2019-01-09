afbbcc30cb65a5ee7a1fe3d9e2147f47509b4eca
Rain Liu <yuliu@outlook.com>
2019-01-09 00:02:27 -0800

    Update 581ShortestUnsortedContinuousSubarray.rs

--- src/581ShortestUnsortedContinuousSubarray/581ShortestUnsortedContinuousSubarray.rs
+++ src/581ShortestUnsortedContinuousSubarray/581ShortestUnsortedContinuousSubarray.rs
@@ -22,3 +22,22 @@ impl SlowSolution {
         }
     }
 }
+
+
+impl Solution {
+    pub fn find_unsorted_subarray(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        let n = nums.len();
+        let mut s = -1;
+        let mut e = -2;
+        let mut max = nums[0];
+        let mut min = nums[n-1];
+        for i in 1..n {
+            max = std::cmp::max(max, nums[i]);
+            min = std::cmp::min(min, nums[n-1-i]);
+            if nums[i] < max {e=i as i32;}
+            if nums[n-1-i]>min {s=(n-1-i) as i32;}
+        }
+        (e-s+1) as i32
+    }
+}
