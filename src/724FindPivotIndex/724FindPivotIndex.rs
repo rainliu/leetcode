7dba5eab2c8a5339aa4db9d693fcec67f677f421
Rain Liu <yuliu@outlook.com>
2019-01-09 23:53:25 -0800

    Create 724FindPivotIndex.rs

---
+++ src/724FindPivotIndex/724FindPivotIndex.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn pivot_index(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut left = vec![0;n];
+        let mut right= vec![0;n];
+        for i in 1..n {
+            left[i] = left[i-1]+nums[i-1];
+            right[n-1-i] = right[n-i]+nums[n-i];
+        }
+        for i in 0..n{
+            if left[i]==right[i]{
+                return i as i32
+            }
+        }
+        -1
+    }
+}
