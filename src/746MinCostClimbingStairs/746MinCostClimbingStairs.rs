475ac8106400658fdb4b5ab4e121861ac7e96390
Rain Liu <yuliu@outlook.com>
2019-01-10 21:51:43 -0800

    Update 746MinCostClimbingStairs.rs

--- src/746MinCostClimbingStairs/746MinCostClimbingStairs.rs
+++ src/746MinCostClimbingStairs/746MinCostClimbingStairs.rs
@@ -1,9 +1,8 @@
 impl Solution {
     pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
-        let n = cost.len();
         let mut f2=cost[0];
         let mut f1=cost[1];
-        for i in 2..n {
+        for i in 2..cost.len() {
             let f0=cost[i]+std::cmp::min(f1, f2);
             f2=f1;
             f1=f0;
