20cac87773cab6f364dc5f57228838db7b7e516a
Rain Liu <yuliu@outlook.com>
2019-01-11 22:45:32 -0800

    Update 914XOfAKindInADeckOfCards.rs

--- src/914XOfAKindInADeckOfCards/914XOfAKindInADeckOfCards.rs
+++ src/914XOfAKindInADeckOfCards/914XOfAKindInADeckOfCards.rs
@@ -28,3 +28,29 @@ impl Solution {
         false
     }
 }
+
+impl Solution2 {
+    pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
+        let mut map:HashMap<i32,i32> = HashMap::new();
+        for num in deck {
+            if let Some(x) = map.get_mut(&num){
+                *x += 1;
+            }else{
+                map.insert(num, 1);
+            }
+        }
+        if let Some(min) = map.values().min(){
+            let mut X = *min;
+            for value in map.values() {
+                X = Self::gcd(X, *value);
+            }
+            X>=2
+        }else{
+            false
+        }
+    }
+    
+    fn gcd(x:i32, y:i32)->i32 {
+        if (x == 0) {y} else {Self::gcd(y%x, x)}
+    }
+}
