0644d6bb0a77791c5d44ea381380a845743541a0
Rain Liu <yuliu@outlook.com>
2019-01-12 23:41:05 -0800

    Update 438FindAllAnagramsInAString.rs

--- src/438FindAllAnagramsInAString/438FindAllAnagramsInAString.rs
+++ src/438FindAllAnagramsInAString/438FindAllAnagramsInAString.rs
@@ -28,3 +28,49 @@ impl Solution {
         result
     }
 }
+
+impl Solution2 {
+    pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
+        let sn = s.len();
+        let pn = p.len();
+        let mut countp = vec![0;26];
+        let pc = p.into_bytes();
+        for c in &pc {
+            countp[(c-'a' as u8) as usize] += 1;
+        }
+        let mut result = vec![];
+        let sc = s.into_bytes();
+        let mut last_match = false;
+        let mut j = pn-1;
+        while j<sn {
+            if !last_match {
+                let mut counts = countp.clone();
+                if Self::is_anagrams(&mut counts, &sc, j-pn+1, j+1) {
+                    last_match = true;
+                    result.push((j-pn+1) as i32);
+                }
+            }else if sc[j as usize]==sc[(j-pn) as usize]{
+                result.push((j-pn+1) as i32);
+            }else{
+                last_match = false;
+            }
+            j+=1;
+        }
+        result
+    }
+    
+    fn is_anagrams(counts: &mut [i32], sc:&[u8], i:usize, j:usize) -> bool {
+        let mut i = i;
+        let mut j = j;
+        while i<j {
+            let k = (sc[i as usize]-'a' as u8) as usize;
+            if counts[k] <= 0 {
+                break;
+            }else{
+                counts[k]-=1;
+            }
+            i+=1;
+        }
+        i==j
+    }
+}
