8b7861b82834ba34136f845198f99a6b8a1337a4
Rain Liu <yuliu@outlook.com>
2019-01-13 21:05:01 -0800

    Update 599MinimumIndexSumOfTwoLists.rs

--- src/599MinimumIndexSumOfTwoLists/599MinimumIndexSumOfTwoLists.rs
+++ src/599MinimumIndexSumOfTwoLists/599MinimumIndexSumOfTwoLists.rs
@@ -1,25 +1,21 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn find_restaurant(list1: Vec<String>, list2: Vec<String>) -> Vec<String> {
-        let mut map1 = HashMap::new();
-        let mut map2 = HashMap::new();
-        for i in 0..list1.len() {
-            map1.insert(&list1[i], i);
-        }
+        let mut map = HashMap::new();
         for j in 0..list2.len() {
-            map2.insert(&list2[j], j);
+            map.insert(&list2[j], j);
         }
         let mut global = list1.len()+list2.len();
-        for (key, val) in map1.iter() {
-            if let Some(x) = map2.get(key) {
-                global = std::cmp::min(global, *val+*x);
+        for i in 0..list1.len() {
+            if let Some(x) = map.get(&list1[i]) {
+                global = std::cmp::min(global, i+*x);
             }
         }
         let mut result = vec![];
-        for (key, val) in map1.iter() {
-            if let Some(x) = map2.get(key) {
-                if *val+*x == global {
-                    result.push((*key).clone());
+        for i in 0..list1.len() {
+            if let Some(x) = map.get(&list1[i]) {
+                if i+*x == global {
+                    result.push(list1[i].clone());
                 }
             }
         }
