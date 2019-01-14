8510b2b90de05c2755dc20c6ecc952662de7910a
Rain Liu <yuliu@outlook.com>
2019-01-13 21:07:58 -0800

    Update 599MinimumIndexSumOfTwoLists.rs

--- src/599MinimumIndexSumOfTwoLists/599MinimumIndexSumOfTwoLists.rs
+++ src/599MinimumIndexSumOfTwoLists/599MinimumIndexSumOfTwoLists.rs
@@ -22,3 +22,26 @@ impl Solution {
         result
     }
 }
+
+impl Solution2 {
+    pub fn find_restaurant(list1: Vec<String>, list2: Vec<String>) -> Vec<String> {
+        let mut map = HashMap::new();
+        for j in 0..list2.len() {
+            map.insert(&list2[j], j);
+        }
+        let mut global = list1.len()+list2.len();
+        let mut result = vec![];
+        for i in 0..list1.len() {
+            if let Some(x) = map.get(&list1[i]) {
+                if i+*x <= global {
+                    if i+*x < global {
+                        result.clear();
+                        global = i+*x;
+                    }
+                    result.push(list1[i].clone());
+                }
+            }
+        }
+        result
+    }
+}
