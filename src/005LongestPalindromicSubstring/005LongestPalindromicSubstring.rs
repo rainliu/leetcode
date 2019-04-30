67a26e4dfefe916377bf161ab6858c6d95ccb462
Rain Liu <yuliu@outlook.com>
2019-04-29 23:56:47 -0700

    Update 005LongestPalindromicSubstring.rs

--- src/005LongestPalindromicSubstring/005LongestPalindromicSubstring.rs
+++ src/005LongestPalindromicSubstring/005LongestPalindromicSubstring.rs
@@ -4,11 +4,11 @@ impl Solution {
         let mut begin = 0;
         let mut end = 0;
         for pos in 0..s.len() {
-            let m = Self::findMirror(s, pos);
-            let c = Self::findCentre(s, pos);
-            if m*2>c && m*2 > end-begin {
-                begin = pos + 1 - m;
-                end = pos + 1 + m;
+            let m = Self::expandCentre(s, pos as i32, pos as i32 + 1);
+            let c = Self::expandCentre(s, pos as i32 - 1, pos as i32 +1);
+            if m>c && m > end-begin {
+                begin = pos + 1 - m/2;
+                end = pos + 1 + m/2;
             }else if c > end-begin {
                 begin = pos - c/2;
                 end = pos + c/2 + 1;
@@ -17,27 +17,15 @@ impl Solution {
         String::from_utf8(s[begin..end].to_vec()).unwrap()
     }
     
-    fn findMirror(s:&[u8], pos: usize) -> usize {
-        let mut i = pos as i32;
-        let mut j = pos as i32 +1;
-        let mut l = 0;
-        while i>=0 && j<s.len() as i32 && s[i as usize] == s[j as usize] {
-            i -= 1;
-            j += 1;
-            l += 1;
-        }
-        l
-    }
-    
-    fn findCentre(s:&[u8], pos: usize) -> usize {
-        let mut i = pos as i32 -1;
-        let mut j = pos as i32 +1;
-        let mut l = 1;
+    fn expandCentre(s:&[u8], i: i32, j: i32) -> usize {
+        let mut i = i;
+        let mut j = j;
+        let mut l = (j-i)/2;
         while i>=0 && j<s.len() as i32 && s[i as usize] == s[j as usize] {
             i -= 1;
             j += 1;
             l += 2;
         }
-        l
+        l as usize
     }
 }
