8cdc9da7fc45b46721c85026d7e5863a6d3c678d
Rain Liu <yuliu@outlook.com>
2019-04-29 21:02:44 -0700

    Create 002AddTwoNumbers.rs

---
+++ src/002AddTwoNumbers/002AddTwoNumbers.rs
@@ -0,0 +1,50 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut dummy = ListNode::new(0);
+        let mut l = &mut dummy;
+        let mut l1 = l1;
+        let mut l2 = l2;
+        let mut c = 0;
+        while let (Some(p1), Some(p2)) = (l1.as_mut(), l2.as_mut()) {
+            let d = p1.val + p2.val + c;
+            c = d/10;
+            l.next = Some(Box::new(ListNode::new(d%10)));
+            l = l.next.as_mut().unwrap();
+            l1 = p1.next.take();
+            l2 = p2.next.take();
+        }
+        let mut l3 = if l1.is_some() {
+            l1
+        }else{
+            l2
+        };
+        while let Some(p3) = l3.as_mut() {
+            let d = p3.val + c;
+            c = d/10;
+            l.next = Some(Box::new(ListNode::new(d%10)));
+            l = l.next.as_mut().unwrap();
+            l3 = p3.next.take();
+        }
+        if c != 0 {
+            l.next = Some(Box::new(ListNode::new(c)));
+        }
+        
+        return dummy.next;
+    }
+}
