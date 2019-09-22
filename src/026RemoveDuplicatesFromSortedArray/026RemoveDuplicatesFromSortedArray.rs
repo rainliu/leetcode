3726be95faa911d9fe56558d419570e3c0a1a9ea
Rain Liu <yuliu@outlook.com>
2019-09-22 08:36:18 -0700

    Update 026RemoveDuplicatesFromSortedArray.rs

--- src/026RemoveDuplicatesFromSortedArray/026RemoveDuplicatesFromSortedArray.rs
+++ src/026RemoveDuplicatesFromSortedArray/026RemoveDuplicatesFromSortedArray.rs
@@ -17,3 +17,25 @@ impl Solution {
         (i+1) as i32
     }
 }
+
+impl Solution2 {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let (mut i, mut j, mut k, n) = (0, 0, 0, nums.len());
+        const d: usize = 1;
+        while j < n {
+            if nums[j] != nums[i-k] {
+                nums.swap(i, j);
+                k = 1;
+                i += 1;
+            } else if nums[j] == nums[i-k] && k < d {
+                nums.swap(i,j);
+                k+=1;
+                i+=1;
+            }
+            
+            j += 1;
+        }
+        
+        i as i32
+    }
+}
