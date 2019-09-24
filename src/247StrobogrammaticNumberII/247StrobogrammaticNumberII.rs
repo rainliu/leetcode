542ab996f412a479430bd39122d6149ba1f2caf5
Rain Liu <yuliu@outlook.com>
2019-09-23 22:59:29 -0700

    Update 247StrobogrammaticNumberII.rs

--- src/247StrobogrammaticNumberII/247StrobogrammaticNumberII.rs
+++ src/247StrobogrammaticNumberII/247StrobogrammaticNumberII.rs
@@ -1,30 +1,20 @@
 impl Solution {
     pub fn find_strobogrammatic(n: i32) -> Vec<String> {
-        if n < 2 {
-            return Solution::find_strobogrammatic_helper(n);
-        }
-        
-        let strobogrammatic = Solution::find_strobogrammatic_helper(n-2);
-        let mut results = vec![];
-        for s in &strobogrammatic{
-            results.push("1".to_string()+s+"1");
-            results.push("6".to_string()+s+"9");
-            results.push("8".to_string()+s+"8");
-            results.push("9".to_string()+s+"6");
-        }
-        results
+        Solution::find_strobogrammatic_helper(n, n)
     }
-    fn find_strobogrammatic_helper(n: i32) -> Vec<String> {
+    fn find_strobogrammatic_helper(n: i32, m: i32) -> Vec<String> {
         if n <=0 {
             return vec!["".to_string()];
         }else if n==1 {
             return vec!["0".to_string(), "1".to_string(), "8".to_string()];
         }
         
-        let strobogrammatic = Solution::find_strobogrammatic_helper(n-2);
+        let strobogrammatic = Solution::find_strobogrammatic_helper(n-2, m);
         let mut results = vec![];
         for s in &strobogrammatic{
-            results.push("0".to_string()+s+"0");
+            if m!=n{
+                results.push("0".to_string()+s+"0");
+            }
             results.push("1".to_string()+s+"1");
             results.push("6".to_string()+s+"9");
             results.push("8".to_string()+s+"8");
