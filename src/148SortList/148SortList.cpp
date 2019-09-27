2994aca16298728051badfed684291efa2a0c350
Rain Liu <yuliu@outlook.com>
2019-09-26 21:38:44 -0700

    Update 148SortList.cpp

--- src/148SortList/148SortList.cpp
+++ src/148SortList/148SortList.cpp
@@ -22,6 +22,50 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+
+class SolutionMerge {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head==nullptr || head->next==nullptr) return head;
+        ListNode* prev = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast!=nullptr && fast->next!=nullptr){
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = nullptr;
+        
+        ListNode*l1 = sortList(head);
+        ListNode*l2 = sortList(slow);
+        return mergeList(l1, l2);
+    }
+    
+    ListNode* mergeList(ListNode* l1, ListNode* l2) {
+        ListNode dummy = ListNode(0);
+        ListNode *p = &dummy;
+        while (l1!=nullptr && l2!=nullptr){
+            if(l1->val < l2->val){
+                p->next = l1;
+                l1 = l1->next;
+            }else{
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if (l1!=nullptr){
+            p->next = l1;
+        }else{
+            p->next = l2;
+        }     
+        
+        return dummy.next;
+    }
+    
+};
+
 class Solution {
 public:
     ListNode *sortList(ListNode *head) {
