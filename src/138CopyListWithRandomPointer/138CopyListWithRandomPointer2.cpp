8e8ddab9bd5274a5da997a78a22c0996afbc35b5
Rain Liu <yuliu@outlook.com>
2019-09-26 19:03:54 -0700

    Update 138CopyListWithRandomPointer2.cpp

--- src/138CopyListWithRandomPointer/138CopyListWithRandomPointer2.cpp
+++ src/138CopyListWithRandomPointer/138CopyListWithRandomPointer2.cpp
@@ -10,7 +10,34 @@ using namespace std;
       RandomListNode *next, *random;
       RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
   };
- 
+
+class Solution2 {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> map;
+        Node dummy = Node(0, nullptr, nullptr);
+        Node* p = &dummy;
+        Node* q = head;
+        while (q != nullptr) {
+            p->next = new Node(q->val, nullptr, nullptr);
+            p = p->next;
+            map.insert({q, p});
+            q = q->next;
+        }
+        q = head;
+        p = dummy.next;
+        while (q != nullptr) {
+            if (q->random != nullptr) {
+                p->random = map[q->random];
+            }
+            q = q->next;
+            p = p->next;
+        }
+        
+        return dummy.next;
+    }
+};
+
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
