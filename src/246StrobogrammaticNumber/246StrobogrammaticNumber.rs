772412aa6617aed5ded7f86663b23f7a6aa18fa0
Rain Liu <yuliu@outlook.com>
2019-09-26 23:44:10 -0700

    Update 246StrobogrammaticNumber.rs

--- src/246StrobogrammaticNumber/246StrobogrammaticNumber.rs
+++ src/246StrobogrammaticNumber/246StrobogrammaticNumber.rs
@@ -6,12 +6,9 @@ impl Solution {
                              //0,1,  2,  3,  4,  5,6,  7,8,9
         
         let num = num.as_bytes();
-        let (mut i, mut j) = (0, num.len() - 1);
+        let (mut i, mut j) = (0 as i32, num.len() as i32 - 1);
         while i <= j {
-            if i==j{
-                return num[i]=='0' as u8 || num[i]=='1' as u8 || num[i]=='8' as u8;    
-            }
-            let (m, n) = (num[i] - ('0' as u8), num[j] - ('0' as u8));
+            let (m, n) = (num[i as usize] - ('0' as u8), num[j as usize] - ('0' as u8));
             if map[m as usize] != n {
                 return false;
             }
