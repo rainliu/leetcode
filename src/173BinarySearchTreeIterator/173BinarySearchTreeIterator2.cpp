0d20a5c34540790815faf9fc76c1a2383c9aec81
Rain Liu <yuliu@outlook.com>
2019-09-26 23:24:53 -0700

    Update 173BinarySearchTreeIterator2.cpp

--- src/173BinarySearchTreeIterator/173BinarySearchTreeIterator2.cpp
+++ src/173BinarySearchTreeIterator/173BinarySearchTreeIterator2.cpp
@@ -40,6 +40,39 @@ public:
   }
 };
 
+class BSTIterator2 {
+private:
+    stack<TreeNode*> s;
+    TreeNode* root;
+public:
+    BSTIterator(TreeNode* root) {
+        this->root = root;
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        int val = 0;
+        while (root!=nullptr || !s.empty()){
+            if (root == nullptr) {
+                root = s.top(); s.pop();
+                val = root->val;
+                root = root->right;
+                break;
+            }else{
+                s.push(root);
+                root = root->left;
+            }
+        }
+        
+        return val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return root != nullptr || !s.empty();
+    }
+};
+
 /**
  * Your BSTIterator will be called like this:
  * BSTIterator i = BSTIterator(root);
