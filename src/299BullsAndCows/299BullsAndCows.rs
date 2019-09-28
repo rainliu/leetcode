8e1f5a0b6bfe414843fdfd4337c2e6c3528c7dcb
Rain Liu <yuliu@outlook.com>
2019-09-27 23:30:30 -0700

    Update 299BullsAndCows.rs

--- src/299BullsAndCows/299BullsAndCows.rs
+++ src/299BullsAndCows/299BullsAndCows.rs
@@ -25,3 +25,30 @@ impl Solution {
         return format!("{}A{}B", bulls, cows);
     }
 }
+
+impl Solution2 {
+    pub fn get_hint(secret: String, guess: String) -> String {
+        let secret = secret.as_bytes();
+        let guess = guess.as_bytes();
+        let mut secret_pos = vec![0; 10];
+        let mut guess_pos = vec![0; 10];
+        
+        let (mut bulls, mut cows) = (0, 0);
+        for i in 0..secret.len() {
+            if secret[i] == guess[i] {
+                bulls += 1;
+            }else{
+                secret_pos[(secret[i] - ('0' as u8)) as usize] += 1;
+                guess_pos [(guess[i]  - ('0' as u8)) as usize] += 1;
+            }
+            
+        }
+        
+        for j in 0..10 {
+            if secret_pos[j] > 0 && guess_pos[j] > 0 {
+                cows  += std::cmp::min(secret_pos[j], guess_pos[j]);
+            }
+        }
+        return format!("{}A{}B", bulls, cows);
+    }
+}
