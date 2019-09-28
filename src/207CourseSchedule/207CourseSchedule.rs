a6e8704a86b9fcfcf5fb5de7f8aebc0f935559c1
Rain Liu <yuliu@outlook.com>
2019-09-27 19:53:46 -0700

    Create 207CourseSchedule.rs

---
+++ src/207CourseSchedule/207CourseSchedule.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+        let mut map:HashMap<i32, Vec<i32>> = HashMap::new();
+        for pair in prerequisites {
+            if let Some(neighbors) = map.get_mut(&pair[0]) {
+                neighbors.push(pair[1]);
+            }else{
+                map.insert(pair[0], vec![pair[1]]);
+            }
+        }
+        let mut visited = vec![0; num_courses as usize];
+        for key in map.keys() {
+            if visited[*key as usize] == 0 {
+                visited[*key as usize] = 1; //visiting
+                if Solution::dfs(&map, &mut visited, *key) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+    
+    fn dfs(map:&HashMap<i32, Vec<i32>>, visited: &mut Vec<i32>, key: i32) -> bool {
+        visited[key as usize] = 2; //visited
+        
+        if let Some(neighbors) = map.get(&key) {
+            for neihbor in neighbors {
+                if visited[*neihbor as usize] == 2 {
+                    return true;
+                }
+                if map.get(neihbor).is_some() {
+                    if Solution::dfs(map, visited, *neihbor) {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        visited[key as usize] = 1; //restore state to visiting
+        return false;
+    }
+}
