f1db1b7e9aedd325ebd8aa2e2cca03ac2d5b7ef4
Rain Liu <yuliu@outlook.com>
2019-10-02 22:48:35 -0700

    Update 560SubarraySumEqualsK.cpp

--- src/560SubarraySumEqualsK/560SubarraySumEqualsK.cpp
+++ src/560SubarraySumEqualsK/560SubarraySumEqualsK.cpp
@@ -1,3 +1,21 @@
+class Solution3 {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        m[0] = 1;
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            if (m.find(sum-k) != m.end()){
+                count += m[sum-k];
+            }
+            m[sum]++;
+        }
+        return count;
+    }
+};
+
 class Solution2 {
 public:
     int subarraySum(vector<int>& nums, int k) {
