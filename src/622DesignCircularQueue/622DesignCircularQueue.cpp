2730fa49ca09de831a6721ffb1bf3bb46cf1b7c5
Rain Liu <yuliu@outlook.com>
2019-10-02 23:04:06 -0700

    Create 622DesignCircularQueue.cpp

---
+++ src/622DesignCircularQueue/622DesignCircularQueue.cpp
@@ -0,0 +1,60 @@
+class MyCircularQueue {
+    vector<int> buffer;
+    int rd;
+    int wt;
+public:
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    MyCircularQueue(int k) {
+        buffer.resize(k+1);
+        rd = 0;
+        wt = 0;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        buffer[wt] = value;
+        wt = (wt+1) % buffer.size();
+        return true;
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    bool deQueue() {
+        if (isEmpty()) return false;
+        rd = (rd+1) % buffer.size();
+        return true;
+    }
+    
+    /** Get the front item from the queue. */
+    int Front() {
+        if (isEmpty()) return -1;
+        return buffer[rd];
+    }
+    
+    /** Get the last item from the queue. */
+    int Rear() {
+        if (isEmpty()) return -1;
+        return buffer[(wt-1+buffer.size())%buffer.size()];
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    bool isEmpty() {
+        return rd==wt;    
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    bool isFull() {
+        return (wt+1)%buffer.size()==rd;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
