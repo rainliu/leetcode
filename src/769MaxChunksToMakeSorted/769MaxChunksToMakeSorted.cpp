ae7f8772d69709e5843c006bbf9d32a1212e2492
Rain Liu <yuliu@outlook.com>
2019-10-03 23:48:07 -0700

    Update 769MaxChunksToMakeSorted.cpp

--- src/769MaxChunksToMakeSorted/769MaxChunksToMakeSorted.cpp
+++ src/769MaxChunksToMakeSorted/769MaxChunksToMakeSorted.cpp
@@ -1,3 +1,17 @@
+class Solution2 {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int count = 0;
+        int max_val = 0;
+        for(int i=0; i<arr.size(); ++i){
+            max_val = max(max_val, arr[i]);
+            if(max_val == i) ++count;
+        }
+        return count;
+    }
+};
+
+
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
