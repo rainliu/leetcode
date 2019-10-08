002f09d96e51d149e7ccb4ac74ecbd1f0f5cdb6c
Rain Liu <yuliu@outlook.com>
2019-10-07 22:26:18 -0700

    Create 951FlipEquivalentBinaryTrees.cpp

---
+++ src/951FlipEquivalentBinaryTrees/951FlipEquivalentBinaryTrees.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        flip(root1);
+        flip(root2);
+        return sameTree(root1, root2);
+    }
+    
+    void flip(TreeNode* root){
+        if(root==nullptr) return;
+        if(root->left==nullptr&root->right==nullptr) return;
+        if(root->left==nullptr || (root->right!=nullptr && root->left->val > root->right->val)) {
+            TreeNode* tmp = root->left;
+            root->left = root->right;
+            root->right = tmp;
+        }
+        flip(root->left);
+        flip(root->right);
+    }
+    
+    bool sameTree(TreeNode* root1, TreeNode* root2) {
+        if(root1==nullptr&&root2==nullptr) return true;
+        if(root1==nullptr&&root2!=nullptr) return false;
+        if(root1!=nullptr&&root2==nullptr) return false;
+        return root1->val==root2->val && sameTree(root1->left, root2->left) && sameTree(root1->right, root2->right);
+    }
+};
