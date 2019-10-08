42575c2f20769209af240210b88ab64a5e0f9a3b
Rain Liu <yuliu@outlook.com>
2019-10-07 22:32:15 -0700

    Update 951FlipEquivalentBinaryTrees.cpp

--- src/951FlipEquivalentBinaryTrees/951FlipEquivalentBinaryTrees.cpp
+++ src/951FlipEquivalentBinaryTrees/951FlipEquivalentBinaryTrees.cpp
@@ -7,6 +7,17 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+class Solution2 {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if(root1==nullptr && root2==nullptr) return true;
+        if(root1==nullptr || root2==nullptr || root1->val != root2->val) return false;
+        return (flipEquiv(root1->left, root2->left)&&flipEquiv(root1->right, root2->right)) ||
+               (flipEquiv(root1->left, root2->right)&&flipEquiv(root1->right, root2->left));
+    }    
+};
+
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
