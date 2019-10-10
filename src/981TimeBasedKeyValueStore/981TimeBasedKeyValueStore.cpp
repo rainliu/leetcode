7c35a105463995e3dba56668c6f13ae61fa5ac6b
Rain Liu <yuliu@outlook.com>
2019-10-09 23:15:27 -0700

    Create 981TimeBasedKeyValueStore.cpp

---
+++ src/981TimeBasedKeyValueStore/981TimeBasedKeyValueStore.cpp
@@ -0,0 +1,25 @@
+class TimeMap {
+    unordered_map<string, vector<pair<int,string>>> m;
+public:
+    /** Initialize your data structure here. */
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = upper_bound(begin(m[key]), end(m[key]), pair<int, string>(timestamp, ""), [](
+        const pair<int, string>& a, const pair<int, string>& b) { return a.first < b.first; });
+        return it == m[key].begin() ? "" : prev(it)->second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
