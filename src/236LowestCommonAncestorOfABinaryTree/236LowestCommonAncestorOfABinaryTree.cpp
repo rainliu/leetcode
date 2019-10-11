6d9f4561820cdd2fdec851f98cbf927b0ffe9761
Rain Liu <yuliu@outlook.com>
2019-10-10 22:04:09 -0700

    Update 236LowestCommonAncestorOfABinaryTree.cpp

--- src/236LowestCommonAncestorOfABinaryTree/236LowestCommonAncestorOfABinaryTree.cpp
+++ src/236LowestCommonAncestorOfABinaryTree/236LowestCommonAncestorOfABinaryTree.cpp
@@ -23,4 +23,33 @@ public:
   }
 };
 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val==p->val || root->val==q->val) return root;
+        bool left = lowestCommonAncestorHelper(root->left, p, q);
+        bool right = lowestCommonAncestorHelper(root->right, p, q);
+        if(left && right) return root;
+        else if(left) return lowestCommonAncestor(root->left, p, q);
+        else return lowestCommonAncestor(root->right, p, q);
+    }
+    
+    bool lowestCommonAncestorHelper(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==nullptr) return false;
+        return root->val==p->val || 
+            root->val==q->val || 
+            lowestCommonAncestorHelper(root->left, p, q) ||
+            lowestCommonAncestorHelper(root->right, p, q);
+    }
+};
 int main() { return 0; }
