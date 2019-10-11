d2ce3b081edf2f6a817c2e4bf5a144e0f2f752d4
Rain Liu <yuliu@outlook.com>
2019-10-10 21:52:54 -0700

    Update 235LowestCommonAncestorOfABinarySearchTree.cpp

--- src/235LowestCommonAncestorOfABinarySearchTree/235LowestCommonAncestorOfABinarySearchTree.cpp
+++ src/235LowestCommonAncestorOfABinarySearchTree/235LowestCommonAncestorOfABinarySearchTree.cpp
@@ -1,3 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val > q->val) return lowestCommonAncestor(root, q, p);
+        if(q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+        if(root->val < p->val) return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
+
+
 #include <iostream>
 using namespace std;
 /**
