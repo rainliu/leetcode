729ba9c39197b3bc4b4e2c0f853b81a2316f83fa
Rain Liu <yuliu@outlook.com>
2019-10-11 22:35:07 -0700

    Update 200NumberOfIslands.cpp

--- src/200NumberOfIslands/200NumberOfIslands.cpp
+++ src/200NumberOfIslands/200NumberOfIslands.cpp
@@ -1,3 +1,69 @@
+class union_find{
+    vector<int> id;
+    vector<int> sz;
+    int cnt;
+public:
+    union_find(int N){
+        cnt = N;
+        id.resize(N);
+        sz.resize(N);
+        for(int i=0; i<N; ++i){
+            id[i]=i;
+            sz[i]=1;
+        }
+    }
+    
+    int find(int i){
+        while(i!=id[i]){
+            id[i]=id[id[i]];
+            i=id[i];
+        }
+        return i;
+    }
+    bool connected(int p, int q){
+        return find(p)==find(q);
+    }
+    void unioning(int p, int q){
+        int i = find(p);
+        int j = find(q);
+        if(i==j) return;
+        else if (sz[i]<sz[j]) {
+            id[i]=j; sz[j]+=sz[i];
+        }else{
+            id[j]=i; sz[i]+=sz[j];
+        }
+        --cnt;
+    }
+    int count(){
+        return cnt;
+    }
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(); if(m==0) return 0;    
+        int n = grid[0].size(); if(n==0) return 0;
+        union_find uf(m*n);
+        int zeros = 0;
+        for(int j=0; j<m; ++j){
+            for(int i=0; i<n; ++i){
+                if(grid[j][i]=='1'){
+                    if(i>0&&grid[j][i-1]=='1'){
+                        uf.unioning(j*n+i, j*n+i-1);
+                    }    
+                    if(j>0&&grid[j-1][i]=='1'){
+                        uf.unioning(j*n+i, (j-1)*n+i);
+                    }
+                }else{
+                    ++zeros;
+                }
+            }
+        }
+        return uf.count()-zeros;
+    }
+};
+
 #include <iostream>
 #include <vector>
 using namespace std;
