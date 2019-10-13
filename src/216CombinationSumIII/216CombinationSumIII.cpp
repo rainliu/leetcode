0a5507984b5b476ac40836814d8ede0ec4b30335
Rain Liu <yuliu@outlook.com>
2019-10-13 16:47:21 -0700

    Update 216CombinationSumIII.cpp

--- src/216CombinationSumIII/216CombinationSumIII.cpp
+++ src/216CombinationSumIII/216CombinationSumIII.cpp
@@ -1,3 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> results;
+        vector<int> a;
+        backtracking(a, 0, k, 9, n, results);
+        return results;
+    }
+    
+    void backtracking(vector<int>& a, int i, int k, int n, int target, vector<vector<int>>& results){
+        if(is_solution(a, i, k, n, target)){
+            process_solution(a, results);
+        }else if(i<n){
+            auto cands = construct_candidates();
+            ++i;
+            for(const auto& cand : cands){
+                if(cand){
+                    a.push_back(i);
+                }
+                backtracking(a, i, k, n, target - (cand ? i : 0), results);
+                if(cand){
+                    a.pop_back();
+                }
+            }
+        }
+    }
+    
+    bool is_solution(vector<int>& a, int i, int k, int n, int target) {
+        return i==n && a.size() == k && 0==target;
+    }
+    
+    void process_solution(vector<int>& a, vector<vector<int>>& results) {
+        results.push_back(a);
+    }
+    
+    vector<bool> construct_candidates(){
+        return vector<bool>{true, false};
+    }
+};
+
 #include <iostream>
 #include <vector>
 using namespace std;
