0f989a0926a2f13c2da55e5468264f9f02f11737
Rain Liu <yuliu@outlook.com>
2019-10-13 13:52:51 -0700

    Update 078Subsets.cpp

--- src/078Subsets/078Subsets.cpp
+++ src/078Subsets/078Subsets.cpp
@@ -1,3 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> results;
+        
+        for(int i=0; i<(1<<nums.size()); ++i){
+            vector<int> result;
+            for(int j=0; j<nums.size();++j){
+                if((i>>j)&0x1) {
+                    result.push_back(nums[j]);
+                }
+            }
+            results.push_back(result);
+        }
+        
+        return results;
+    }
+};
+
 #include <sstream>
 #include <iostream>
 #include <vector>
