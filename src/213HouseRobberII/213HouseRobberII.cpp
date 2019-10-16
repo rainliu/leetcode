7060196b1208d5ab48e1e43db2467a3dbe20a096
Rain Liu <yuliu@outlook.com>
2019-10-15 19:20:03 -0700

    Update 213HouseRobberII.cpp

--- src/213HouseRobberII/213HouseRobberII.cpp
+++ src/213HouseRobberII/213HouseRobberII.cpp
@@ -1,3 +1,24 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        return max(rob(nums, 0, n-2), rob(nums, 1,n-1));
+    }
+    
+    int rob(vector<int>& nums, int lo, int hi){
+        //D[i]=max(nums[i]+D[i-2], D[i-1])
+        vector<int> D(3, 0);
+        for(int i=lo; i<=hi; ++i){
+            D[0]=max(nums[i]+D[2], D[1]);
+            D[2]=D[1];
+            D[1]=D[0];
+        }
+        return D[0];
+    }
+};
+
 #include <iostream>
 #include <vector>
 using namespace std;
