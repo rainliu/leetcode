12514d39e41e82f1a47899700158579b28b58b69
Rain Liu <yuliu@outlook.com>
2019-10-15 23:10:36 -0700

    Update 190ReverseBits.cpp

--- src/190ReverseBits/190ReverseBits.cpp
+++ src/190ReverseBits/190ReverseBits.cpp
@@ -1,3 +1,26 @@
+class Solution {
+    uint8_t lut[256];
+public:
+    Solution(){
+        for(int i=0; i<256; ++i){
+            uint8_t r = 0;
+            for(int j=0; j<8; ++j){
+                r |= ((i>>j)&0x1)<<(7-j);
+            }
+            lut[i]= r;
+        }
+    }
+    uint32_t reverseBits(uint32_t n) {
+        uint8_t* ptr = (uint8_t*)&n;
+        uint32_t result = 0;
+        for(int i=0; i<4; ++i){
+            result |= lut[*ptr++]<<(3-i)*8;    
+        }
+        return result;
+    }
+};
+
+
 #include <iostream>
 using namespace std;
 
