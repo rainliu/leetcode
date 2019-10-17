764e42d3bbb002dc34d7f9f61da0f5261e0a21f2
Rain Liu <yuliu@outlook.com>
2019-10-16 17:42:17 -0700

    Update 1066CampusBikesII.cpp

--- src/1066CampusBikesII/1066CampusBikesII.cpp
+++ src/1066CampusBikesII/1066CampusBikesII.cpp
@@ -7,6 +7,42 @@ public:
                 distances[i][j] = abs(workers[i][0]-bikes[j][0]) + abs(workers[i][1]-bikes[j][1]);
             }
         }   
+        
+        vector<vector<int>> D(workers.size(), vector<int>(1<<10, -1));
+        return backtracking(distances, D, 0, workers.size(), 0);
+    }
+    
+    int backtracking(vector<vector<int>>& distances, vector<vector<int>>& D, int k, int n, int mask) {
+        if(k==n) return 0;
+        if(D[k][mask]!=-1){
+            return D[k][mask];
+        }
+        
+        int global = numeric_limits<int>::max();
+        for(int j=0; j<distances[0].size(); ++j){
+            if(mask&(1<<j)){
+                continue;
+            }
+            mask |= 1<<j;
+            int local = distances[k][j] + backtracking(distances, D, k+1, n, mask);
+            global = min(global, local);
+            mask &= ~(1<<j);
+        }
+        
+        D[k][mask] = global;
+        return D[k][mask];
+    }
+};
+
+class Solution {
+public:
+    int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        vector<vector<int>> distances(workers.size(), vector<int>(bikes.size(), 0));
+        for(int i=0; i<workers.size(); ++i){
+            for(int j=0; j<bikes.size(); ++j){
+                distances[i][j] = abs(workers[i][0]-bikes[j][0]) + abs(workers[i][1]-bikes[j][1]);
+            }
+        }   
         int global = numeric_limits<int>::max();
         vector<int> a;
         backtracking(a, 0, workers.size(), distances, global);
