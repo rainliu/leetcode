b5b9cd6a384ccc7b90d02d098b15380467be0b23
Rain Liu <yuliu@outlook.com>
2019-10-16 22:34:35 -0700

    Create 792NumberOfMatchingSubsequences.cpp

---
+++ src/792NumberOfMatchingSubsequences/792NumberOfMatchingSubsequences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numMatchingSubseq(string S, vector<string>& words) {
+        unordered_map<string, bool> m;
+        int count = 0;
+        for(const auto& word : words){
+            bool r;
+            if(m.find(word) == m.end()){
+                r = is_subseq(S, word);
+                m[word]=r;
+            }else{
+                r = m[word];
+            }
+            count += r;
+            
+        }
+        return count;
+    }
+    
+    bool is_subseq(const string& S, const string& word){
+        int n=S.size();
+        int m=word.size();
+        int i=0;
+        int j=0;
+        while(i<n&&j<m){
+            if(S[i]==word[j]){
+                ++j;
+            }
+            ++i;
+        }
+        return j==m;
+    }
+};
