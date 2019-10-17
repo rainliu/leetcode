65e1cb3d23933042daed1dde054c60df006b965a
Rain Liu <yuliu@outlook.com>
2019-10-16 22:18:25 -0700

    Update 289GameOfLife.cpp

--- src/289GameOfLife/289GameOfLife.cpp
+++ src/289GameOfLife/289GameOfLife.cpp
@@ -1,3 +1,40 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(); if(m==0) return;
+        int n = board[0].size(); if(n==0) return;
+        for(int j=0; j<m; ++j){
+            for(int i=0; i<n; ++i){
+                int c = 0;
+                for(int v=-1; v<=1; ++v){
+                    for(int u=-1; u<=1; ++u){
+                        if(u==0&&v==0) continue;
+                        int x = i+u;
+                        int y = j+v;
+                        if(x>=0&&x<n&&y>=0&&y<m){
+                            c += board[y][x]&0x1;
+                        }
+                    }
+                }
+                if(board[j][i]&0x1){
+                    if(c==2||c==3){
+                        board[j][i] |= 0x2;
+                    }
+                }else{
+                    if(c==3){
+                        board[j][i] |= 0x2;
+                    }
+                }
+            }
+        }
+        for(int j=0; j<m; ++j){
+            for(int i=0; i<n; ++i){
+                board[j][i]>>=1;
+            }
+        }
+    }
+};
+
 #include <vector>
 using namespace std;
 
