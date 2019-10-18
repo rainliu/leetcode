043281eac5177a77898a5a79958ac179b6cbbe96
Rain Liu <yuliu@outlook.com>
2019-10-17 22:15:14 -0700

    Update 240SearchA2DMatrixII.cpp

--- src/240SearchA2DMatrixII/240SearchA2DMatrixII.cpp
+++ src/240SearchA2DMatrixII/240SearchA2DMatrixII.cpp
@@ -1,3 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(); if(m==0) return false;
+        int n = matrix[0].size(); if(n==0) return false;
+        int row = m-1;
+        int col = 0;
+        while(col>=0&&col<n&&row>=0&&row<m){
+            if(matrix[row][col]==target) return true;
+            else if (matrix[row][col]>target) --row;
+            else ++col;
+        }
+            
+        return false;
+    }
+};
+
 #include <iostream>
 #include <vector>
 using namespace std;
