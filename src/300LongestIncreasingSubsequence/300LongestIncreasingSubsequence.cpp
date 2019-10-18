185f28d970400873395f27fb5f2a312e5b744703
Rain Liu <yuliu@outlook.com>
2019-10-17 22:38:46 -0700

    Update 300LongestIncreasingSubsequence.cpp

--- src/300LongestIncreasingSubsequence/300LongestIncreasingSubsequence.cpp
+++ src/300LongestIncreasingSubsequence/300LongestIncreasingSubsequence.cpp
@@ -1,3 +1,22 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> D(nums.size(), 0);
+        int global = 0;
+        for(int j=0; j<nums.size(); ++j){
+            int local = 0;
+            for(int i=0; i<j; ++i){
+                if(nums[i]<nums[j]){
+                    local = max(local, D[i]);
+                }
+            }
+            D[j]=1+local;
+            global = max(global, D[j]);
+        }
+        return global;
+    }
+};
+
 #include <vector>
 using namespace std;
 
