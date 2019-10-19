f00b0a7146f364ae08eabea457cbc50281965ec8
Rain Liu <yuliu@outlook.com>
2019-10-19 15:49:07 -0700

    Update 211AddAndSearchWord.cpp

--- src/211AddAndSearchWord/211AddAndSearchWord.cpp
+++ src/211AddAndSearchWord/211AddAndSearchWord.cpp
@@ -1,3 +1,68 @@
+struct Node{
+    bool val;
+    vector<Node*> next;
+    Node(){
+        next.resize(26, nullptr);
+        val = false;
+    }
+};
+
+class WordDictionary {
+    Node* root;
+
+private:
+    Node* put(Node* x, string& word, int d){
+        if(x==nullptr) x = new Node();
+        if(d==word.size()) {
+            x->val = true;
+        }else{
+            int c = word[d]-'a';
+            x->next[c] = put(x->next[c], word, d+1);
+        }
+        return x;
+    }
+    
+    bool get(Node* x, string& word, int d){
+        if(x==nullptr) return false;
+        if(d==word.size()){
+            return x->val;
+        }else if(word[d]=='.'){
+            for(int c=0; c<26; ++c){
+                if(get(x->next[c], word, d+1)){
+                    return true;
+                }
+            }    
+            return false;
+        }else{
+            int c = word[d] - 'a';
+            return get(x->next[c], word, d+1);
+        }
+    }
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        root=nullptr;
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        root = put(root, word, 0);
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        return get(root, word, 0);   
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+
 #include <iostream>
 using namespace std;
 
