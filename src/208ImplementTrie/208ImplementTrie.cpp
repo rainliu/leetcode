5a8329107df1f04e0f076dfba0de521678031109
Rain Liu <yuliu@outlook.com>
2019-10-19 15:34:36 -0700

    Update 208ImplementTrie.cpp

--- src/208ImplementTrie/208ImplementTrie.cpp
+++ src/208ImplementTrie/208ImplementTrie.cpp
@@ -1,3 +1,68 @@
+struct Node{
+    bool val;
+    vector<Node*> next;
+    Node(){
+        next.resize(26, nullptr);
+        val=false;
+    }
+};
+class Trie {
+    Node* root=nullptr;
+    
+private:
+    Node* put(Node* x, string& word, int d){
+        if(x==nullptr) {x = new Node();}
+        if(d==word.size()) {
+            x->val = true;
+        }else{
+            int c = word[d]-'a';
+            x->next[c] = put(x->next[c], word, d+1);
+        }
+        return x;
+    }
+    Node* get(Node* x, string& word, int d){
+        if(x==nullptr) return nullptr;
+        if(d==word.size()){
+          return x;  
+        }else{
+            int c = word[d]-'a';
+            return get(x->next[c], word, d+1);
+        }
+    }
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        root=put(root, word, 0);    
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Node* x = get(root, word, 0);
+        if(x==nullptr) return false;
+        return x->val;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Node* x = get(root, prefix, 0);
+        if(x==nullptr) return false;
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+
 #include <iostream>
 using namespace std;
 
