a59a304257874c30f155ffa00cfe7114174b7d48
Rain Liu <yuliu@outlook.com>
2019-10-20 20:06:19 -0700

    Create 785IsGraphBipartite.cpp

---
+++ src/785IsGraphBipartite/785IsGraphBipartite.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        unordered_set<int> colormap[2];
+        for(int i=0; i<graph.size(); ++i){
+            if(colormap[0].find(i)==colormap[0].end()&&
+               colormap[1].find(i)==colormap[1].end()){
+                queue<pair<int,int>> q;               
+                q.push({i, 0});
+                colormap[0].insert(i);
+                while(!q.empty()){
+                    auto [v, color] = q.front(); q.pop();
+                    for(const auto& u : graph[v] ){
+                        if(colormap[color].find(u)!=colormap[color].end()){
+                            return false;
+                        }
+                        if(colormap[1-color].find(u)==colormap[1-color].end()){
+                            q.push({u, 1-color});
+                            colormap[1-color].insert(u);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
