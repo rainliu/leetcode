6a4322ae13767b999356851df8806054531157e8
Rain Liu <yuliu@outlook.com>
2019-10-22 23:10:07 -0700

    Update 621TaskScheduler.cpp

--- src/621TaskScheduler/621TaskScheduler.cpp
+++ src/621TaskScheduler/621TaskScheduler.cpp
@@ -1,3 +1,42 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> m;
+        for(const auto& task : tasks){
+            ++m[task];
+        }
+        auto cmp = [](const auto& a, const auto& b) {
+            return a.second < b.second;
+        };
+        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> q(cmp);
+        for(const auto& p : m){
+            q.push(p);
+        }
+        int result = 0;
+        while(!q.empty()){
+            queue<pair<char, int>> t;
+            int local = min(int(q.size()), n+1);                        
+            for(int i=0; i<local; ++i){
+                auto p = q.top(); q.pop();
+                if(--p.second > 0){
+                    t.push(p);
+                }
+            }
+            while(!t.empty()){
+                q.push(t.front());
+                t.pop();
+            }
+            
+            if(q.empty()){
+                result += local;
+            }else{
+                result += n+1;
+            }
+        }        
+        return result;
+    }
+};
+
 #include <iostream>
 using namespace std;
 
