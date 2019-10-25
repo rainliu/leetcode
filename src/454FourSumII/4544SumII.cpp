114c2b8a6c9ef7df0cad5bd1761e609512181978
Rain Liu <yuliu@outlook.com>
2019-10-24 23:34:42 -0700

    Update 4544SumII.cpp

--- src/454FourSumII/4544SumII.cpp
+++ src/454FourSumII/4544SumII.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
-        unordered_map<int, int> mab;
-        unordered_map<int, int> mcd;
+        unordered_map<int, int> m;
         int n = A.size();
         for(int i=0; i<n; ++i){
             for(int j=0; j<n; ++j){
-                ++mab[A[i]+B[j]];
-                ++mcd[C[i]+D[j]];
+                ++m[A[i]+B[j]];
             }
         }
         int count = 0;
-        for(const auto& [ab, c] : mab){
-            if(mcd.find(-ab)!=mcd.end()){
-                count+=c*mcd[-ab];
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<n; ++j){
+                int target = C[i]+D[j];
+                auto it = m.find(-target);
+                if(it!=m.end()){
+                    count+=it->second;
+                }
             }
         }
         
         return count;
     }
 };
-
 #include <iostream>
 #include <unordered_map>
 #include <vector>
