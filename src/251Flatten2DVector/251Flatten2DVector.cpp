67b2755ed6af77ddbe45d132f4c0ebad37e53fc4
Rain Liu <yuliu@outlook.com>
2019-10-24 19:18:16 -0700

    Update 251Flatten2DVector.cpp

--- src/251Flatten2DVector/251Flatten2DVector.cpp
+++ src/251Flatten2DVector/251Flatten2DVector.cpp
@@ -1,3 +1,53 @@
+class Vector2D {
+    vector<vector<int>>::iterator it;
+    vector<int>::iterator itt;
+    vector<vector<int>>::iterator it_end;
+public:
+    Vector2D(vector<vector<int>>& v) {
+        it = v.begin();
+        it_end = v.end();
+        while(it!=it_end){
+            itt = it->begin();
+            if(itt==it->end()){
+                ++it;
+            }else{
+                break;
+            }
+        }
+    }
+    
+    int next() {
+        if(!hasNext()) return 0;
+        
+        int val = *itt;
+        ++itt;
+        if(itt==it->end()){
+            ++it;
+            while(it!=it_end){
+                itt = it->begin();
+                if(itt==it->end()){
+                    ++it;
+                }else{
+                    break;
+                }
+            }
+        }
+        
+        return val;
+    }
+    
+    bool hasNext() {
+        return it != it_end;
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D* obj = new Vector2D(v);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+
 #include <iostream>
 #include <vector>
 using namespace std;
