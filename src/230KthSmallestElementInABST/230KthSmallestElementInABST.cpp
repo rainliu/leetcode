6fb3437724feea527665daccc6cc25f8ac6b4674
Rain Liu <yuliu@outlook.com>
2019-10-24 19:00:33 -0700

    Update 230KthSmallestElementInABST.cpp

--- src/230KthSmallestElementInABST/230KthSmallestElementInABST.cpp
+++ src/230KthSmallestElementInABST/230KthSmallestElementInABST.cpp
@@ -1,3 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        if(root==nullptr || k<=0) return 0;
+        int left = count(root->left);
+        if(k == left+1) return root->val;
+        else if(k>left+1){
+            return kthSmallest(root->right, k-left-1);
+        }else{
+            return kthSmallest(root->left, k);
+        }
+    }
+    
+    int count(TreeNode* root){
+        if(root==nullptr) return 0;
+        return 1+count(root->left)+count(root->right);
+    }
+};
+
 #include <iostream>
 using namespace std;
 
