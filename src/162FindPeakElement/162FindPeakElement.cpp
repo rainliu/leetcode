38ee56e635a229140cc2ca5a14126be44107a2f5
Rain Liu <yuliu@outlook.com>
2019-10-27 21:06:30 -0700

    Update 162FindPeakElement.cpp

--- src/162FindPeakElement/162FindPeakElement.cpp
+++ src/162FindPeakElement/162FindPeakElement.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0;
+        int hi = n-1;
+        while(lo<=hi){
+            int mi = lo + (hi-lo)/2;
+            if( (mi-1<0||nums[mi-1]<nums[mi])&&
+                (mi+1>n-1||nums[mi]>nums[mi+1]) ){
+                return mi;
+            }else if( (mi-1<0||nums[mi-1]<nums[mi])&&
+                      (mi+1<=n-1&&nums[mi]<nums[mi+1]) ){
+                lo = mi+1;
+            }else{
+                hi = mi-1;
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
         int lo = 0;
         int hi = int(nums.size())-1;
         while(lo<=hi){
